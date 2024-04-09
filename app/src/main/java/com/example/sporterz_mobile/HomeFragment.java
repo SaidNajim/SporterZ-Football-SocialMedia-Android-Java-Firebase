473e9a87de8fe3cec8e5d722c72e06824e69338f
Mohammed-Yassine BOUMEHDI <myboumehdi.2002@gmail.com>
2024-04-09 13:33:25 +0000

    Make possible for users to upload their photos

--- app/src/main/java/com/example/sporterz_mobile/HomeFragment.java
+++ app/src/main/java/com/example/sporterz_mobile/HomeFragment.java
@@ -2,6 +2,8 @@ package com.example.sporterz_mobile;
 
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -21,7 +23,11 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
 
+import java.io.File;
+import java.io.IOException;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
@@ -44,6 +50,8 @@ public class HomeFragment extends Fragment {
     private ArrayList<HomeItem> items;
     private HomeItem item;
     private String username;
+    private Bitmap imageBitmap;
+    private StorageReference storageReference;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -85,7 +93,12 @@ public class HomeFragment extends Fragment {
                 SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm", Locale.getDefault());
                 Date currentDate = new Date();
                 String formattedDate = dateFormat.format(currentDate);
-                HomeItem homeItem = new HomeItem(username, thinking, formattedDate);
+                try {
+                    getProfileImage();
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+                HomeItem homeItem = new HomeItem(imageBitmap, username, thinking, formattedDate);
                 String postId = databaseReference.push().getKey();
                 if (!thinking.isEmpty() && !postId.isEmpty()) {
                     databaseReference.child(postId).setValue(homeItem)
@@ -109,16 +122,26 @@ public class HomeFragment extends Fragment {
         return view;
     }
 
+    private void getProfileImage() throws IOException {
+        final String userId = auth.getCurrentUser().getUid().toString();
+        storageReference = FirebaseStorage.getInstance().getReference().child("images/" + userId);
+        File localfile = File.createTempFile("tempImage", "jpeg");
+        storageReference.getFile(localfile).addOnSuccessListener(taskSnapshot -> {
+            imageBitmap = BitmapFactory.decodeFile(localfile.getAbsolutePath());
+        });
+    }
+
     private void fetchPosts() {
         dbReference.addListenerForSingleValueEvent(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
                 items.clear();
                 for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
+                    Bitmap postImage = (Bitmap) snapshot.child("imageBitmap").getValue();
                     String username = String.valueOf(snapshot.child("username").getValue());
                     String thinking = String.valueOf(snapshot.child("thinking").getValue());
                     String postDate = String.valueOf(snapshot.child("postDate").getValue());
-                    item = new HomeItem(username, thinking, postDate);
+                    item = new HomeItem(postImage, username, thinking, postDate);
                     items.add(item);
                 }
                 myAdapter.notifyDataSetChanged();
