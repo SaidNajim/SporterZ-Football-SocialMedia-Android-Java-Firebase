349492f2313d9c76645dbb3d0fcf16c2be2eab25
Said NAJIM <saidnajim.ma@gmail.com>
2024-04-29 11:10:16 +0100

    adding my matches fragment things

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -44,8 +44,6 @@ dependencies {
     implementation("com.google.firebase:firebase-auth:22.3.1")
     implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.6.1")
     implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.6.1")
-    implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.6.1")
-    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.6.1")
     implementation("com.google.firebase:firebase-database:20.3.1")
     implementation("de.hdodenhof:circleimageview:3.1.0")
     implementation("com.google.firebase:firebase-storage:20.3.0")
@@ -54,5 +52,7 @@ dependencies {
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
     implementation(platform("com.google.firebase:firebase-bom:32.8.0"))
     implementation("com.google.firebase:firebase-analytics")
-
+    implementation("com.github.bumptech.glide:glide:4.12.0")
+    annotationProcessor("com.github.bumptech.glide:compiler:4.12.0")
+    implementation("com.android.volley:volley:1.2.1")
 }
--- app/src/main/java/com/example/sporterz_mobile/MatchesFragment.java
+++ app/src/main/java/com/example/sporterz_mobile/MatchesFragment.java
@@ -1,6 +1,7 @@
 package com.example.sporterz_mobile;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -9,8 +10,20 @@ import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
 import java.util.ArrayList;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
+import java.util.Locale;
 
 public class MatchesFragment extends Fragment {
 
@@ -33,8 +46,20 @@ public class MatchesFragment extends Fragment {
         // Initialize your matches list
         matchesList = new ArrayList<>();
 
-        // Populate the matches list with sample data
-        populateMatchesList();
+        // Populate the matches list
+        String url = "https://match-service-1hpr.onrender.com/match-service/getAll";
+        JsonArrayRequest jsonArrayRequest = new JsonArrayRequest(Request.Method.GET, url, null, new Response.Listener<JSONArray>() {
+            @Override
+            public void onResponse(JSONArray response) {
+                populateMatchesList(response);
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError volleyError) {
+                Log.e("Error", volleyError.toString());
+            }
+        });
+        Volley.newRequestQueue(getContext().getApplicationContext()).add(jsonArrayRequest);
 
         // Initialize adapter with the matches list
         adapter = new MyMatchesAdapter(getContext(), matchesList);
@@ -45,9 +70,41 @@ public class MatchesFragment extends Fragment {
         return view;
     }
 
-    private void populateMatchesList() {
-        // Sample data for demonstration
-        matchesList.add(new MatchesDataClass("Tue. 09/04, 19:00", "Stade Santiago-Bernabéu", "Madrid, Espagne", "Real Madrid", "3", "https://s2.ezgif.com/tmp/ezgif-2-150ce314ca.png", "Manchester City", "3", "https://download.logo.wine/logo/Manchester_City_F.C./Manchester_City_F.C.-Logo.wine.png"));
-        matchesList.add(new MatchesDataClass("Wed. 17/04, 20:00", "Stade Santiago-Bernabéu", "Madrid, Espagne", "Real Madrid", "0", "https://s2.ezgif.com/tmp/ezgif-2-150ce314ca.png", "Manchester City", "0", "https://download.logo.wine/logo/Manchester_City_F.C./Manchester_City_F.C.-Logo.wine.png"));
+    private void populateMatchesList(JSONArray response) {
+        try {
+            for (int i = 0; i < response.length(); i++) {
+                JSONObject jsonObject = response.getJSONObject(i);
+
+                String idAwayTeam = jsonObject.getString("idAwayTeam");
+                String idHomeTeam = jsonObject.getString("idHomeTeam");
+                String dateTime = jsonObject.getString("datetimeMatch");
+                SimpleDateFormat originalFormat = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSSXXX", Locale.getDefault());
+                Date date = null;
+                try {
+                    date = originalFormat.parse(dateTime);
+                } catch (Exception e) {
+                    Log.e("Date Parse Exception", e.toString());
+                }
+
+                // Format Date object into desired format
+                SimpleDateFormat desiredFormat = new SimpleDateFormat("E dd/MM, HH:mm", Locale.getDefault());
+                String formattedDatetime = desiredFormat.format(date);
+
+                matchesList.add(new MatchesDataClass(
+                        formattedDatetime,
+                        jsonObject.getString("nameStadium"),
+                        jsonObject.getString("cityStadium"),
+                        jsonObject.getString("nameHomeTeam"),
+                        String.valueOf(jsonObject.getInt("goalsHomeTeam")),
+                        "https://media.api-sports.io/football/teams/" + idHomeTeam +".png",
+                        jsonObject.getString("nameAwayTeam"),
+                        String.valueOf(jsonObject.getInt("goalsAwayTeam")),
+                        "https://media.api-sports.io/football/teams/" + idAwayTeam +".png" )
+                );
+            }
+        } catch (Exception e) {
+            Log.e("Error", e.toString());
+        }
+        adapter.notifyDataSetChanged();
     }
 }
